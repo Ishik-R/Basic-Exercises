49df4552524b0eb26e1a6744995116df608f5ef4
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-07-26 19:31:21 -0300

    Add files via upload

---
+++ curso-python-aula-04.py
@@ -0,0 +1,18 @@
+# Curso Python #04 - Primeiros comandos em Python3
+# https://www.youtube.com/watch?v=31llNGKWDdo
+
+# Exercício 1: Hello usuário
+nome = input('Qual é o seu nome? ')
+print('Olá ', nome, ', prazer em conhecê-lo.')
+
+# Exercício 2: exemplo de exibição de dados
+dia = 24
+mes = 'abril'
+ano = 1998
+print('Você nasceu em ', dia, ' de ', mes, ' de ', ano)
+
+# Exercício 3: exibição da soma de dois valores
+num1 = float(input('Primeiro número: '))
+num2 = float(input('Segundo número: '))
+sum = num1 + num2
+print('Resultado da soma: %.1f' %sum)
