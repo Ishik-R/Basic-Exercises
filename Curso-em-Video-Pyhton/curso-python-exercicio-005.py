8a4f21c54139cb634fa919fd734777a237a4f425
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-07-28 23:39:15 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/curso-python-exercicio-005.py
@@ -0,0 +1,5 @@
+# Exercício Python #005 - Antecessor e Sucessor
+# https://www.youtube.com/watch?v=664e0G_S9nU
+
+num = int(input('Insira um número inteiro: '))
+print(f'O numero anterior ao {num} é {num-1} \nO número sucessor ao {num} é {num+1}')
