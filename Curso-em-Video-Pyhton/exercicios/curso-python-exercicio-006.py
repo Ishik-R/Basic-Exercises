021a4941878a370bef04a31587960b793c25c0d7
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 20:31:57 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-006.py
@@ -0,0 +1,7 @@
+# Exercício Python #006 - Dobro, Triplo, Raiz Quadrada
+# https://www.youtube.com/watch?v=mqcNw_dhl8I
+
+num = int(input('Insira um número inteiro: '))
+print(f'O dobro de {num} é {num*2}')
+print(f'O triplo de {num} é {num*3}')
+print(f'A raiz quadrada de {num} vale {round(num**0.5, 3)}')
