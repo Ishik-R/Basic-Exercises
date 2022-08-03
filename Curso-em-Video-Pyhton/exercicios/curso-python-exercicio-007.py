0253bad2f0ba442b6e3cc080f24dfae6dfe36772
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 20:40:09 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-007.py
@@ -0,0 +1,6 @@
+# Exercício Python #007 - Média Aritmética
+# https://www.youtube.com/watch?v=_QfISzy0IKs
+
+n1 = float(input('Insira o valor da nota 1: '))
+n2 = float(input('Insira o valor da nota 2: '))
+print(f'A média da nota 1 ({n1:.1f}) e da nota 2 ({round(n2, 1)}) é {round((n1+n2)/2, 1)}')
