be9e1bfa82ea03aed191c01576c01e8cf1c43f6d
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 23:06:32 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-014.py
@@ -0,0 +1,5 @@
+# Exercício Python #014 - Conversor de Temperaturas
+# https://www.youtube.com/watch?v=9l_Gay8BuAw
+
+t = float(input('Insira uma temperatura (em graus Celsius, °C): '))
+print(f'A temperatura de {t:.1f}°C corresponde a {(t*1.8)+32:.1f}°F')
