25091aff3a27648efdd026e4605cb88efcf503e2
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 21:33:15 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-010.py
@@ -0,0 +1,8 @@
+# Exercício Python #010 - Conversor de Moedas
+# https://www.youtube.com/watch?v=xM4AX3Lp2mo
+
+n = float(input('Quanto dinheiro você possui na sua carteira? '))
+
+print(f'Com R${n} você pode comprar US${n/5.28:.2f}')
+
+# Considerando a cotação de 03/08/2022 de US$1 = R$5,28
