ed40f52fd62fd7eeda466ab14a1c43a2f1ca59e5
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 21:53:19 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-013.py
@@ -0,0 +1,5 @@
+# Exercício Python #013 - Reajuste Salarial
+# https://www.youtube.com/watch?v=cTkivN8XcJ0
+
+s = float(input('Informe o valor de um salário: R$ '))
+print(f'Um funcionário que ganhava R$ {s:.2f}, com um reajuste de 15%, passará a ganhar R$ {s*1.15:.2f}')
