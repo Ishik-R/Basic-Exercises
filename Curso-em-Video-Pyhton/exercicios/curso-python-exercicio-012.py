5fde7c95f9ed3d196fa4dbd633b6eafeedb31cc6
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-03 21:47:49 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-012.py
@@ -0,0 +1,6 @@
+# Exercício Python #012 - Calculando Descontos
+# https://www.youtube.com/watch?v=4MAmKOT9FeU
+
+p = float(input('Insira o valor de um produto: R$ '))
+
+print(f'O produto que custava R${p:.2f}, ao receber um desconto de 5% passará a custar R${p*0.95:.2f}')
