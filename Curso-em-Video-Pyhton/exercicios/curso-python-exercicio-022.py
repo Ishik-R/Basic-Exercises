226eaa2d5c98e42157a33f2b4a57e7bf038b7ed0
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-22 17:58:08 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-022.py
@@ -0,0 +1,18 @@
+# Exercício Python #022 - Analisador de Textos
+# https://www.youtube.com/watch?v=EQQt-6QqXOs
+
+# Crie um programa que leia o nome completo de uma pessoa e mostre: 
+# - O nome com todas as letras maiúsculas e minúsculas.
+# - Quantas letras ao todo (sem considerar espaços).
+# - Quantas letras tem o primeiro nome.
+
+print('\nEXERCÍCIO 22 - Analisador de Nomes')
+print('--' * 16)
+
+nome = input(str('Digite seu ome completo: '))
+pNome = nome.split()[0]
+
+print(f'Exibição de todas as letras sendo maiúsculas: {nome.upper()}')
+print(f'Exibição de todas as letras sendo minúsculas: {nome.lower()}')
+print(f'Quatidade total de caracteres sem considerar espaços: {len(nome.replace(" ",""))}')
+print(f'Número de caracteres no primeiro nome: {len(pNome)}')
