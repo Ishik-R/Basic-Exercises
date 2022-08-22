8ee250d6c19f8ce255a87c0311694de4bfc75d8d
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-22 18:22:33 -0300

    Update curso-python-exercicio-024.py

--- Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-024.py
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-024.py
@@ -1,7 +1,7 @@
 # Exercício Python #024 - Verificando as primeiras letras de um texto
 # https://www.youtube.com/watch?v=QroT8cZMRnc
 
-# Exercício Python 023: Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+# Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
 
 print('\nEXERCÍCIO 024 - Verificando as primeiras letras de um texto')
 print('--' * 16)
