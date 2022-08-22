c37c7222b2d2f15a50b9d5e8b4f59301c5aab225
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-22 19:48:46 -0300

    Update curso-python-exercicio-027.py

--- Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-027.py
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-027.py
@@ -1,18 +1,10 @@
-# Exercício Python #028 - Jogo da Adivinhação v.1.0
-# https://www.youtube.com/watch?v=kchC5KLZSZ4
+# Exercício Python #027 - Primeiro e último nome de uma pessoa
+# https://www.youtube.com/watch?v=SifYYsXhLM8
 
-# Exercício Python 028: Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5.
-#  Ele deve pedir para o usuário tentar descobrir qual foi o número escolhido. 
-#  O programa deverá escrever na tela se o usuário venceu ou perdeu.
-print("\n#028 - Jogo da Adivinhação v.1.0")
+# Exercício Python 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+print("\n#027 - Primeiro e último nome de uma pessoa")
 print("--" * 16)
 
-from random import randint
+nome = input("Escreva seu nome completo: ").title().split()
 
-n = int(input("Tente adivinhar o número inteiro escolhido entre 0 e 5: "))
-nComputador = randint(0, 5)
-
-if n == nComputador:
-    print("Você acertou!\n")
-else:
-    print(f"Você errou. O número sorteado foi {nComputador}\n")
+print(f"Olá {nome[0]}, seu último nome é {nome[len(nome)-1]} \n")
