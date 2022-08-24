7eecfce0fbb542a31ae3e0d9e5ae76a56ddf26d4
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2022-08-24 19:34:44 -0300

    Add files via upload

---
+++ Curso-em-Video-Pyhton/exercicios/curso-python-exercicio-047.py
@@ -0,0 +1,10 @@
+# Exercício Python #047 - Contagem de pares
+# https://www.youtube.com/watch?v=Qws8-E-YrlY
+
+# Exercício Python 047: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+print('\n#047 - Contagem de pares')
+print('--' * 16)
+print('Exibindo todos os números pares entre 1 e 50:')
+for c in range(2, 51, 2):
+    print(c)
