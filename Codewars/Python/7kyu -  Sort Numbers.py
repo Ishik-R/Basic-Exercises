e3881a6c627c075f2490dba3ee881ba075849a5b
Ishik-R <82295100+Ishik-R@users.noreply.github.com>
2023-03-15 03:03:55 +0100

    Update 7kyu -  Sort Numbers.py

--- Codewars/Python/7kyu -  Sort Numbers.py
+++ Codewars/Python/7kyu -  Sort Numbers.py
@@ -1,3 +1,13 @@
+'''
+7 kyu - Sort Numbers
+https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
+Python
+
+Finish the solution so that it sorts the passed in array of numbers.
+
+If the function passes in an empty array or null/nil value then it should return an empty array.
+'''
+
 def solution(nums):
     if nums:
         return sorted(nums)
@@ -5,6 +15,6 @@ def solution(nums):
         return []
 
 sol1 = solution([1,2,3,10,5])
+
 print(sol1) # should return [1,2,3,5,10]
 print(solution(None)) # should return []
- 
